fc71bf325fb495034449312a028097d25af85740
Olena Honcharuk <gasmin07@gmail.com>
2021-08-26 22:18:07 +0300

    Initial dist subtree commit

---
+++ precache-manifest.64d29dff1ff9d3c91e74b0ee66bd090f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bce22bb77f31d3fd503f4a9b8252f8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0ffbae2a3ead80c9fd2",
+    "url": "/js/app.c56946df.js"
+  },
+  {
+    "revision": "4d723094db8b8eba97f4",
+    "url": "/js/chunk-vendors.aabc6d62.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
